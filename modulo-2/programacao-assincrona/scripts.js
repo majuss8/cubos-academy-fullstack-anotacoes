2fffeab7eb32b4bd01ee8647be6a2baa1d16bb44
Maria Júlia <mjuliamary0@gmail.com>
2023-12-09 17:38:53 -0300

    Código Assíncrono

--- modulo-2/programacao-assincrona/scripts.js
+++ modulo-2/programacao-assincrona/scripts.js
@@ -1,8 +1,26 @@
+const { getCityFromZipcode } = require('utils-playground');
+
 // Código Síncrono
+// console.log('início');
+
+// for (let index = 1; index < 6; index++) {
+//     console.log(index);
+// }
+
+// console.log('fim');
+
+// Código Assíncrono
 console.log('início');
 
-for (let index = 1; index < 6; index++) {
-    console.log(index);
-}
+getCityFromZipcode('60810010').then(cidade => { // função assíncrona
+    console.log(cidade);
+});
 
 console.log('fim');
+
+const cidade = getCityFromZipcode('60810010');
+
+console.log(cidade);
+
+/* Promise { <pending> } */
+
