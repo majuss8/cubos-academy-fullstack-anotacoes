262636a6e451cd7926326662e8d690b090f14e78
Maria Júlia <mjuliamary0@gmail.com>
2023-12-09 21:04:50 -0300

    Promise all

--- modulo-2/programacao-assincrona/api/src/index.js
+++ modulo-2/programacao-assincrona/api/src/index.js
@@ -4,8 +4,14 @@ const { getCityFromZipcode } = require('utils-playground');
 const app = express();
 
 app.get('/', async (req, res) => { 
-    const cidade = await getCityFromZipcode('44380000');
-    res.send(`A cidade encontrada foi: ${cidade}`);
+    const cidade = getCityFromZipcode('44380000');
+    const cidade2 = getCityFromZipcode('41256250');
+
+    const promise = await Promise.all([cidade, cidade2])
+
+    const [ res1, res2 ] = promise;
+
+    res.send(`A cidades encontrada foram: ${res1} e ${res2}`);
 });
 
 app.listen(3000);
