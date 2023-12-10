0c3f06300b050205b75a0c16f49379f986335a8c
Maria Júlia <mjuliamary0@gmail.com>
2023-12-09 21:15:23 -0300

    exercício 1

--- modulo-2/programacao-assincrona/api/src/index.js
+++ modulo-2/programacao-assincrona/api/src/index.js
@@ -1,17 +1,25 @@
 const express = require('express');
-const { getCityFromZipcode } = require('utils-playground');
+const { getCityFromZipcode, getPackageDescriptionNpm } = require('utils-playground');
 
 const app = express();
 
-app.get('/', async (req, res) => { 
-    const cidade = getCityFromZipcode('44380000');
-    const cidade2 = getCityFromZipcode('41256250');
+// app.get('/', async (req, res) => { 
+//     const cidade = getCityFromZipcode('44380000');
+//     const cidade2 = getCityFromZipcode('41256250');
 
-    const promise = await Promise.all([cidade, cidade2])
+//     const promise = await Promise.all([cidade, cidade2])
 
-    const [ res1, res2 ] = promise;
+//     const [ res1, res2 ] = promise;
 
-    res.send(`A cidades encontrada foram: ${res1} e ${res2}`);
+//     res.send(`A cidades encontrada foram: ${res1} e ${res2}`);
+// });
+
+app.get('/pacote/:nomePacote', async (req, res) => {
+    const { nomePacote } = req.params;
+
+    const descricaoPacote = await getPackageDescriptionNpm(nomePacote);
+
+    return res.send(descricaoPacote)
 });
 
 app.listen(3000);
